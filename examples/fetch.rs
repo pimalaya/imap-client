d75733ed17f6ec912fc747a79fb0a71ea7ed9ee2
Damian Poddebniak <poddebniak@mailbox.org>
2024-09-04 20:59:59 +0200

    docs: add `fetch` example

---
+++ examples/fetch.rs
@@ -0,0 +1,54 @@
+use imap_client::{
+    imap_types::{
+        fetch::{Macro, MacroOrMessageDataItemNames, MessageDataItem},
+        sequence::SequenceSet,
+    },
+    Client,
+};
+
+const USAGE: &str =
+    "USAGE: cargo run --example=fetch -- <host> <port> <username> <password>";
+
+#[tokio::main]
+async fn main() {
+    let (host, port, username, password) = {
+        let mut args = std::env::args();
+        let _ = args.next();
+
+        (
+            args.next().expect(USAGE),
+            str::parse::<u16>(&args.next().expect(USAGE)).unwrap(),
+            args.next().expect(USAGE),
+            args.next().expect(USAGE),
+        )
+    };
+
+    let mut client = Client::tls(host, port).await.unwrap();
+
+    client.authenticate_plain(username, password).await.unwrap();
+
+    let select_data = client.select("inbox").await.unwrap();
+    println!("{select_data:?}\n");
+
+    let data = client
+        .fetch(
+            SequenceSet::try_from("1:10").unwrap(),
+            MacroOrMessageDataItemNames::Macro(Macro::Full),
+        )
+        .await
+        .unwrap();
+
+    println!("# INBOX\n");
+    for (_, items) in data {
+        let envelope = items
+            .as_ref()
+            .iter()
+            .find(|item| matches!(item, MessageDataItem::Envelope(_)))
+            .unwrap();
+        if let MessageDataItem::Envelope(env) = envelope {
+            if let Some(sub) = &env.subject.0 {
+                println!("* {:?}", std::str::from_utf8(sub.as_ref()).unwrap());
+            }
+        }
+    }
+}
